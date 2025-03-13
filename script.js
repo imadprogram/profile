cda6a84e55e2a5f9ed2d898f964fab5c3a5092f3
imadprogram <98602726+imadprogram@users.noreply.github.com>
2025-03-13 07:42:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// let name = document.querySelectorAll(".name");
+
+// for (let i = 0; i < name.length; i++) {
+//     name[0].style.color = "red";
+// }
+
+// let button = document.querySelector("button");
+
+
+// button.addEventListener('mouseover', function(){
+//     button.classList.add('add');
+// })
+
+// button.addEventListener('click', function(){
+//     button.style.color = 'red'
+// })
